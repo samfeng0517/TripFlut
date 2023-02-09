ff2e5cef2375d691e19d4fabdb14e5e5eff2c690
Sam Feng <sam0880517@gmail.com>
2023-02-09 14:26:24 +0800

    feat(trip_card): add Inkwell

--- lib/features/trip_list/view/trip_card.dart
+++ lib/features/trip_list/view/trip_card.dart
@@ -16,66 +16,68 @@ class TripCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Card(
       clipBehavior: Clip.antiAlias,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AspectRatio(
-            aspectRatio: 16 / 9,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(12),
-                  bottomRight: Radius.circular(12),
+      child: InkWell(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(12),
+                    bottomRight: Radius.circular(12),
+                  ),
+                  color: HexColor(trip.color),
                 ),
-                color: HexColor(trip.color),
+                width: double.infinity,
+                child: trip.imageUrl != null
+                    ? CachedNetworkImage(
+                        imageUrl: trip.imageUrl!,
+                        placeholder: (context, url) => Shimmer.fromColors(
+                          baseColor: HexColor(trip.color),
+                          highlightColor: Theme.of(context).highlightColor,
+                          child: const SizedBox.expand(),
+                        ),
+                      )
+                    : null,
               ),
-              width: double.infinity,
-              child: trip.imageUrl != null
-                  ? CachedNetworkImage(
-                      imageUrl: trip.imageUrl!,
-                      placeholder: (context, url) => Shimmer.fromColors(
-                        baseColor: HexColor(trip.color),
-                        highlightColor: Theme.of(context).highlightColor,
-                        child: const SizedBox.expand(),
-                      ),
-                    )
-                  : null,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              trip.title,
-              style: Theme.of(context).textTheme.displaySmall,
-              maxLines: 2,
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                trip.title,
+                style: Theme.of(context).textTheme.displaySmall,
+                maxLines: 2,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
-            child: Row(
-              children: [
-                Text(
-                  DateFormat.Md().format(trip.startTime),
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                Icon(
-                  Icons.arrow_forward,
-                  size: Theme.of(context).textTheme.titleLarge?.fontSize,
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                Text(
-                  DateFormat.Md().format(trip.endTime),
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+              child: Row(
+                children: [
+                  Text(
+                    DateFormat.Md().format(trip.startTime),
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Icon(
+                    Icons.arrow_forward,
+                    size: Theme.of(context).textTheme.titleLarge?.fontSize,
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    DateFormat.Md().format(trip.endTime),
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
