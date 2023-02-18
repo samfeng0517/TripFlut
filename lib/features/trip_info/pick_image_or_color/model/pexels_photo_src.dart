5c18917ec99c980708ad373899e7513d3ed62472
Sam Feng <sam0880517@gmail.com>
2023-02-18 18:50:54 +0800

    feat(pexels_photo_src): add doc comments

--- lib/features/trip_info/pick_image_or_color/model/pexels_photo_src.dart
+++ lib/features/trip_info/pick_image_or_color/model/pexels_photo_src.dart
@@ -1,11 +1,27 @@
+///An assortment of different image sizes that can be used to display this Pexels photo.
 class PexelsPhotoSrc {
+  ///The image without any size changes.
   String? original;
+
+  ///The image resized to `W 940px X H 650px` `DPR 2`.
   String? large2x;
+
+  ///The image resized to `W 940px X H 650px` `DPR 1`.
   String? large;
+
+  ///The image scaled proportionally so that it's new height is `350px`.
   String? medium;
+
+  ///The image scaled proportionally so that it's new height is `130px`.
   String? small;
+
+  ///The image cropped to `W 800px X H 1200px`.
   String? portrait;
+
+  ///The image cropped to `W 1200px X H 627px`.
   String? landscape;
+
+  ///The image cropped to `W 280px X H 200px`.
   String? tiny;
 
   PexelsPhotoSrc(
